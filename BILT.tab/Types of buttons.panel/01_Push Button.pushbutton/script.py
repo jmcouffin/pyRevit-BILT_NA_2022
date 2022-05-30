661a8bddfb6ff62067ef97b8229a69455f1d59b7
Jean-Marc Couffin <jmcouffin@gmail.com>
2022-05-30 11:16:27 +0200

    pushbutton

--- BILT.tab/Types of buttons.panel/01_Push Button.pushbutton/script.py
+++ BILT.tab/Types of buttons.panel/01_Push Button.pushbutton/script.py
@@ -1,4 +1,5 @@
 from pyrevit import script
+from pyrevit import revit
 
 # grab existing windows
 output = script.get_output()
@@ -10,6 +11,9 @@ output.center()
 # print a picture in a window
 output.print_md('# No Revit was hurt in the process!')
 output.print_image("C:\pyRevit\pyBiltNA.extension\BILT.tab\Types of buttons.panel\\01_Push Button.pushbutton\meme.jpg")
+
+# let's do something with revit at the Application Level
+output.print_md('You are using the following revit build {}'.format(revit.HOST_APP.build))
 
 # automatically close the window
 output.self_destruct(15)
